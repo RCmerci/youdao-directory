d3251cf1a71751de3964b6504e98629c2d20a3f9
czy <562837353@qq.com>
2014-02-14 22:05:57 +0800

    Update BasicDir.h

--- BasicDir.h
+++ BasicDir.h
@@ -1,5 +1,5 @@
 #ifndef __BASIC__
 #define __BASIC__
 //the data interface.
-char *APIkey = "http://fanyi.youdao.com/openapi.do?keyfrom=1qazxcv&key=428531278&type=data&doctype=json&version=1.1&q=";
+char *APIkey = "http://fanyi.youdao.com/openapi.do?keyfrom=XXXXXXXX&key=XXXXXXXXX&type=data&doctype=json&version=1.1&q=";
 #endif
