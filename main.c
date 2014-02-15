78c5b9783c1ff8e9f49c1b5ac117baaac20e1431
RCmerci <562837353@qq.com>
2014-02-15 14:09:50 +0800

    加上持续功能

--- main.c
+++ main.c
@@ -3,13 +3,16 @@
  * 2014-2-12 
  */
 #include "GatherStuff.h"
+int keep_alive = 0;
 int simple_flag = 1;
-char *str_to_trans;
+char str_to_trans[150];
 TRANS trans_res;
+/*--------------------------------------------------------*/
 void help_print(){
     printf("usage:\n");
     printf("\t -f  print full-version translation\n");
-    printf("\t -q \"string to translate\"\n");
+    printf("\t -q  \"string to translate\"\n");
+    printf("\t -k  keep-alive,not single use.\n");
 }
 void argv_check(int argc,char **argv){
     if(argc<2){
@@ -17,7 +20,7 @@ void argv_check(int argc,char **argv){
         exit(EXIT_FAILURE);
     }
     int res,q_time=0;
-    while((res = getopt(argc, argv, "q:f"))!=-1){
+    while((res = getopt(argc, argv, "q:fk"))!=-1){
         switch (res){
             case 'q':
                 if(optarg == NULL){
@@ -25,9 +28,12 @@ void argv_check(int argc,char **argv){
                     exit(EXIT_FAILURE);
                 }
                 ++q_time;
-                str_to_trans = optarg;break;
+                memcpy(str_to_trans,optarg,strlen(optarg)+1);break;
             case 'f':
                 simple_flag = 0;break;
+            case 'k':
+                ++q_time;
+                keep_alive = 1;break;
             default:
                 help_print();
                 exit(EXIT_FAILURE);
@@ -37,7 +43,11 @@ void argv_check(int argc,char **argv){
         help_print();
         exit(EXIT_FAILURE);
     }
-
+}
+size_t write_func(char *ptr, size_t size, size_t nmemb, void *userdata){
+    memcpy(userdata, ptr, size*nmemb);
+    ((char *)userdata)[size*nmemb] = '\0';
+    return size*nmemb;
 }
 void convert_space(char *p1){
     char *p2,p3[100];
@@ -46,15 +56,10 @@ void convert_space(char *p1){
     while(p2!=NULL){
     strcat(p3,p2);
     strcat(p3," ");
-    p2 = strtok(NULL,"-/;");
+    p2 = strtok(NULL,"-");
     }
     p3[strlen(p3)-1] = '\0';
     memcpy(p1,p3,strlen(p3));
-}
-size_t write_func(char *ptr, size_t size, size_t nmemb, void *userdata){
-    memcpy(userdata, ptr, size*nmemb);
-    ((char *)userdata)[size*nmemb] = '\0';
-    return size*nmemb;
 }
 void * trans_thread(void *unuse){
     CURLcode retcode;
@@ -67,6 +72,7 @@ void * trans_thread(void *unuse){
     sprintf(buf, "%s", APIkey);
     convert_space(str_to_trans);
     convert_str = curl_easy_escape(curl,str_to_trans,strlen(str_to_trans));
+    
     strcat(buf, convert_str);
     curl_easy_setopt(curl, CURLOPT_URL, buf);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_func);
@@ -94,8 +100,19 @@ int main(int argc, char **argv){
     pthread_t thread_1;
     int ret_code;
     argv_check(argc,argv);
-    pthread_create(&thread_1, NULL, trans_thread, NULL);
-    ret_code = pthread_join(thread_1, NULL);
+    if(keep_alive == 0){
+        pthread_create(&thread_1, NULL, trans_thread, NULL);
+        ret_code = pthread_join(thread_1, NULL);
+    }
+    else{
+        while(1){
+            printf(">>>");
+            fflush(stdout);
+            fgets(str_to_trans,150,stdin);
+            pthread_create(&thread_1, NULL, trans_thread, NULL);
+            ret_code = pthread_join(thread_1, NULL);
+        }
+    }
     //printf("retcode:%d\n",ret_code);
     return 0;
 }
